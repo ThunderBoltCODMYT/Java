55ccc9af52ca6324867b609410dd63452efe50c5
ThunderBoltCODMYT <aramgade@gmail.com>
2025-10-07 17:23:01 +0530

    Add files via upload

---
+++ p13.java
@@ -0,0 +1,11 @@
+public class p13{
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Using a for-each loop to print each element
+        System.out.println("Elements of the array:");
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
